9e6634dbdbed82d68e7d52678f09d6301c070671
tonyn4444 <tntuyen@ucdavis.edu>
2018-05-12 14:28:25 -0700

    configure google strategy with passport

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const app = express();
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
-app.get('/', (req, res) => {
-	res.send({ bye: 'buddy' });
-});
+passport.use(
+	new GoogleStrategy({
+		clientID: keys.googleClientID,
+		clientSecret: keys.googleClientSecret,
+		callbackURL: '/auth/google/callback',
+	}, (accessToken) => {
+		console.log(accessToken);
+	})
+);
 
-const PORT = process.env.port || 3000;
+app.get(
+	'/auth/google', 
+	passport.authenticate('google', {
+		scope: ['profile', 'email']
+	})
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
+
+const PORT = process.env.port || 5000;
 
 app.listen(PORT, () => {
 	console.log('Server listening on port:', PORT)
