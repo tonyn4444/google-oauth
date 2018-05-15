9d3b64937955a577390d9c36d50949dbaec3f26e
tonyn4444 <tntuyen@ucdavis.edu>
2018-05-14 22:57:36 -0700

    refactor auth routes and passport module into separate files

--- index.js
+++ index.js
@@ -1,29 +1,8 @@
 const express = require('express');
 const app = express();
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys');
 
-passport.use(
-	new GoogleStrategy({
-		clientID: keys.googleClientID,
-		clientSecret: keys.googleClientSecret,
-		callbackURL: '/auth/google/callback',
-	}, (accessToken, refreshToken, profile, done) => {
-		console.log('accessTokoken:', accessToken);
-		console.log('refreshToken:', refreshToken);
-		console.log('profile:', profile);
-	})
-);
-
-app.get(
-	'/auth/google', 
-	passport.authenticate('google', {
-		scope: ['profile', 'email']
-	})
-);
-
-app.get('/auth/google/callback', passport.authenticate('google'));
+require('./server/passport');
+require('./server/authRoutes')(app);
 
 const PORT = process.env.port || 5000;
 
